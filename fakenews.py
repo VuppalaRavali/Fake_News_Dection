f277c73c4e697158dd763ac0084a9ecce5bfd5e0
VuppalaRavali <107422614+VuppalaRavali@users.noreply.github.com>
2022-06-13 23:19:38 +0530

    updated fakenews.py

--- fakenews.py
+++ fakenews.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import pandas as pd
+import joblib
+
 
 text_input = st.text_input("Enter the News","")
 
 text_area_input = st.text_area("Result")
+
+if st.button("Submit"):
+    
+    # Unpickle classifier
+    clf = joblib.load("finalized_model.pkl")
+    
+    # input to lower
+    X = text_input.lower()    
+    # Get prediction
+    prediction = [[clf.predict(X)]]
+    
+    # Output prediction
+    st.text(f"This news is a {prediction}")
